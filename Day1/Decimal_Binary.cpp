bdef5445346646b5a9e353226eec203fecc2a630
Lakshyaaa <56066951+Lakshyaaa@users.noreply.github.com>
2022-09-15 23:15:20 +0530

    Cerner_2^5_2022 - Convert Decimal to Binary Number.

---
+++ Day1/Decimal_Binary.cpp
@@ -0,0 +1,28 @@
+//"cerner_2^5_2022"
+
+// Convert Decimal Number to Binary Number.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+         int A[10];
+         int Num;
+         int i;
+         cout << "Enter the Decimal Number:  ";
+         cin >> Num;
+
+         for (i = 0; Num > 0; i++)
+         {
+                  A[i] = Num % 2;
+                  Num = Num / 2;
+         }
+
+         cout << "Binary of the given Decimal Number:  ";
+
+         for (i = i - 1; i >= 0; i--)
+         {
+                  cout << A[i];
+         }
+         return 0;
+}
