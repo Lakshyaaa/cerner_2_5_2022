a130052e19121923a40b54e835f6c70ed908457e
Lakshyaaa <56066951+Lakshyaaa@users.noreply.github.com>
2022-09-16 16:19:29 +0530

    Cerner_2^5_2022 - Reverse an String using Recursion

--- Day2/Reverse.py
+++ Day2/Reverse.py
@@ -3,11 +3,13 @@
 # Program to reverse an string/Number using recursion
 
 def rev_string(S):
-    if len(S) == 0:
+    if len(S) == 0:  #Checking the lenght of string
         return S
     else:
+        #!=0, the reverse function is recursively called to slice the part of the string except the first character 
+        # and then concatenate the first character to the end of the sliced string.
         return rev_string(S[1:]) + S[0]   
-
-S = input("Enter the string = ")
-print("\nOriginal String = ", S)
+    
+S = input("Enter the string = ")    #User must enter the string or number.
+print("\nOriginal String = ", S)    #Print Function 
 print("\nReversed String = ", rev_string(S))
