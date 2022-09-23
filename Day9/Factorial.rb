36770d90e9f28a1a2e89af223370ef55d8b76b02
Lakshyaaa <56066951+Lakshyaaa@users.noreply.github.com>
2022-09-23 18:44:44 +0530

    Cerner_2^5_2022 - Find Factorial of a given number

---
+++ Day9/Factorial.rb
@@ -0,0 +1,22 @@
+=begin 
+"cerner_2^5_2022" 
+
+"cerner_2tothe5th_2022"
+
+Program to find the factorial of a given number.
+=end
+
+puts "Enter the number = "
+num=gets.chomp.to_i
+
+fact=1
+if (num==0)
+	puts "Error! Could not find the factorial of one"
+else
+	i=1
+	while(i<=num)
+		fact=fact*i
+		i+=1
+	end
+end
+		puts "factorial of #{num} is #{fact}"
