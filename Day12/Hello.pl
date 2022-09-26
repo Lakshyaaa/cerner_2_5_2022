49c43dea7778cfc87f8f3e2af3e2fd782358b7db
Lakshyaaa <56066951+Lakshyaaa@users.noreply.github.com>
2022-09-26 14:30:58 +0530

    Cerner_2^5_2022 - Print.

---
+++ Day12/Hello.pl
@@ -0,0 +1,11 @@
+#"cerner_2^5_2022" 
+#"cerner_2tothe5th_2022"
+
+#!/usr/bin/perl
+  
+# Modules used
+use strict;
+use warnings;
+  
+# Print function 
+print("---------------HELLO CODERs---------------\n");
