705a645fd29e72f17d1765ae2f0f8baffc32b1a8
Lakshyaaa <56066951+Lakshyaaa@users.noreply.github.com>
2022-09-29 09:36:01 +0530

    Cerner_2^5_2022 - Sum of two number using Nested Function.

---
+++ Day15/Sum.r
@@ -0,0 +1,14 @@
+#"cerner_2^5_2022"
+#"cerner_2tothe5th_2022"
+
+# Sum of two number using nested function.
+
+Outer_func <- function(x) {
+  Inner_func <- function(y) {
+    a <- x + y
+    return(a)
+  }
+  return (Inner_func)
+}
+output <- Outer_func(50) # To call the Outer_func
+output(500)
