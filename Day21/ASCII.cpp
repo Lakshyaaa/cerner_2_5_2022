03dcb57be4f3b2369bc6f847592dcdfc47e73e14
Lakshyaaa <56066951+Lakshyaaa@users.noreply.github.com>
2022-10-05 19:16:14 +0530

    Cerner_2^5_2022 - Print ASCII Value.

---
+++ Day21/ASCII.cpp
@@ -0,0 +1,14 @@
+//"cerner_2^5_2022" "cerner_2tothe5th_2022"
+
+// Print ASCII Value.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+ char c;
+ cout << "Enter a character = ";
+ cin >> c;
+ cout << "ASCII Value of " << c << " is " << int(c);
+ return 0;
+}
