c6316920544ca169f2ac1f0a0ca3ac2483b49529
Lakshyaaa <56066951+Lakshyaaa@users.noreply.github.com>
2022-10-09 16:25:15 +0530

    Cerner_2^5_2022 - Distance between Two Points.

---
+++ Day25/measure_distance.cs
@@ -0,0 +1,17 @@
+//"cerner_2^5_2022" "cerner_2tothe5th_2022"
+//Find Distance between Two Points.
+using System;
+
+namespace measure_distance {
+         class Program {
+                  static void Main (string[] args) {
+                           double x1, x2, y1, y2;
+                           x1 = 12d;
+                           x2 = 13d;
+                           y1 = 11d;
+                           y2 = 10d;
+                           var distance = Math.Sqrt ((Math.Pow (x1 - x2, 2) + Math.Pow (y1 - y2, 2)));
+                           Console.WriteLine (distance);
+                  }
+         }
+}
