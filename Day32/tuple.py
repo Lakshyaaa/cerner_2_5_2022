ec561979016ad71b655e4154dc8071152efdd9d9
Lakshyaaa <56066951+Lakshyaaa@users.noreply.github.com>
2022-10-16 18:12:47 +0530

    Cerner_2^5_2022 - Different types of tuples.

---
+++ Day32/tuple.py
@@ -0,0 +1,27 @@
+#"cerner_2^5_2022"
+#"cerner_2tothe5th_2022"
+
+print("-----------------------------Different types of tuples-----------------------------")
+# Different types of tuples
+
+# Empty tuple
+print("---------------Empty Tuple---------------")
+my_tuple = ()
+print(my_tuple)
+
+# Tuple having integers
+print("---------------Tuple having integers---------------")
+my_tuple = (1, 2, 3)
+print(my_tuple)
+
+
+# tuple with mixed datatypes
+print("---------------tuple with mixed datatypes---------------")
+my_tuple = (1, "Hello", 3.4)
+print(my_tuple)
+
+# nested tuple
+print("---------------nested tuple---------------")
+
+my_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+print(my_tuple)
